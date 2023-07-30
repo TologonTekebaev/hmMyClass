3ca2105c5cb4699c6c0b33f8a94bb00e8295e15a
tologon <tologon.tkbv@gmail.com>
2023-07-31 03:39:06 +0600

    progect

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        MyClass obj1 = new MyClass("Tolgon", "Tekebaev",
+                26, "Yssyk-Kol", 183);
+
+        System.out.println("First constructor: \n" +
+                "User first name: " + obj1.firstName +
+                "\n User last name: " + obj1.lastName +
+                "\n User age: " + obj1.age +
+                "\n User region: " + obj1.region +
+                "\n User height: " + obj1.height + "\n");
+
+        MyClass obj2 = new MyClass("Tologon", "Tekebaev",
+                26, "Yssyk-Kol", new String[]{"Java", "English", "Soft Skills", "Tech talk"});
+
+        System.out.println("Second constructor: \n" +
+                "User first name: " + obj2.firstName +
+                "\n User last name: " + obj2.lastName +
+                "\n User age: " + obj2.age +
+                "\n User region: " + obj2.region +
+                "\n User lessons: " + Arrays.toString(obj2.myLessons));
+
     }
 }
--- src/MyClass.java
+++ src/MyClass.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class MyClass {
+public class MyClass {
+    String firstName;
+    String lastName;
+    int age;
+    String region;
+    float height;
+    String[] myLessons;
+
+
+    public MyClass(){
+
+    }
+
+    public MyClass (String firstName, String lastName,
+                   int age, String region, float height) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.region = region;
+        this.height = height;
+    }
+
+    public MyClass(String firstName, String lastName,
+                   int age, String region, String[] myLessons) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.region = region;
+        this.myLessons = myLessons;
+    }
 }
